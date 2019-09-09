66dc220ff37295b40200cd8dc3775ea2dc16c97d
penguab <a19880433@gmail.com>
2019-09-09 14:39:27 -0500

    v0

--- CCSkit_scripts/breakpoint_sort.py
+++ CCSkit_scripts/breakpoint_sort.py
@@ -4,6 +4,7 @@ import sys,re
 def breakpoint_sort(split):
 	breakpoint_file=open(split+".sort","w")
 	breakpoint=[]
+	new_breakpoint=[]
 	with open(split) as f:
 		while True:
 			l= f.readline().rstrip()
@@ -23,23 +24,45 @@ def breakpoint_sort(split):
 			elif line[4]=='right':
 				left='0'
 				right='1'
-			breakpoint.append([line[0],line[1],line[2],'INS','-',hp,left,right,'1'])
-	breakpoint_sorted=sorted(breakpoint,key=lambda x: (x[5],x[0],int(x[1])))
+			breakpoint.append([line[0],line[1],line[2],'INS','-',hp,left,right,'1',left])
+	breakpoint_sorted=sorted(breakpoint,key=lambda x: (x[5],x[9],x[0],int(x[1])))
 	pre=[]
 	for x in range(len(breakpoint_sorted)):
 		if len(pre)==0:
 			pre=breakpoint_sorted[x]
 			continue
-		if breakpoint_sorted[x][5]==pre[5] and breakpoint_sorted[x][0]==pre[0] and int(breakpoint_sorted[x][1])-int(pre[1])<=20 and breakpoint_sorted[x][3]==pre[3]:
+		if breakpoint_sorted[x][5]==pre[5] and breakpoint_sorted[x][9]==pre[9] and  breakpoint_sorted[x][0]==pre[0] and int(breakpoint_sorted[x][1])-int(pre[1])<=20 and breakpoint_sorted[x][3]==pre[3]:
 			pre[6]=str(int(pre[6])+int(breakpoint_sorted[x][6]))
 			pre[7]=str(int(pre[7])+int(breakpoint_sorted[x][7]))
 			pre[8]=str(int(pre[8])+int(breakpoint_sorted[x][8]))
 		else:
-			if int(pre[6])>=3 and int(pre[7])>=3:
-				breakpoint_file.write(('\t'.join(str(n) for n in pre[0:6]+[pre[8]]))+'\n')
+			if int(pre[6])>=3 or int(pre[7])>=3:
+				new_breakpoint.append(pre)
 			pre=breakpoint_sorted[x]
-	if int(pre[6])>=3 and int(pre[7])>=3:
-		breakpoint_file.write(('\t'.join(str(n) for n in pre[0:6]+[pre[8]]))+'\n')
+	if int(pre[6])>=3 or int(pre[7])>=3:
+		new_breakpoint.append(pre)
+	new_breakpoint_sorted=sorted(new_breakpoint,key=lambda x: (x[5],x[0],int(x[1])))
+	for i in range(len(new_breakpoint_sorted)):
+		print(new_breakpoint_sorted[i])
+	new_pre=[]
+	for x in range(len(new_breakpoint_sorted)):
+		if len(new_pre)==0:
+			new_pre=new_breakpoint_sorted[x]
+			continue
+		if new_breakpoint_sorted[x][5]==new_pre[5] and new_breakpoint_sorted[x][0]==new_pre[0] and int(new_breakpoint_sorted[x][1])-int(new_pre[1])<=1000 and new_breakpoint_sorted[x][3]==new_pre[3]:
+			new_pre[6]=str(int(new_pre[6])+int(new_breakpoint_sorted[x][6]))
+			new_pre[7]=str(int(new_pre[7])+int(new_breakpoint_sorted[x][7]))
+			new_pre[8]=str(int(new_pre[8])+int(new_breakpoint_sorted[x][8]))
+		elif new_breakpoint_sorted[x][5]==new_pre[5] and new_breakpoint_sorted[x][0]==new_pre[0] and int(new_breakpoint_sorted[x][1])-int(new_pre[1])>1000 and new_breakpoint_sorted[x][3]==new_pre[3] and int(new_pre[6])>=3 and int(new_pre[7])<=0 and int(new_breakpoint_sorted[x][6])<=0 and int(new_breakpoint_sorted[x][7])>=3:
+			tmp=[new_pre[0],new_pre[1],new_breakpoint_sorted[x][1],'DEL',int(new_breakpoint_sorted[x][1])-int(new_pre[1]),new_pre[5],new_pre[6],new_breakpoint_sorted[x][7],int(new_pre[6])+int(new_breakpoint_sorted[x][7])]
+			breakpoint_file.write(('\t'.join(str(n) for n in tmp))+'\n')
+			new_pre=new_breakpoint_sorted[x]
+		else:
+			if int(new_pre[6])>=3 and int(new_pre[7])>=3:
+				breakpoint_file.write(('\t'.join(str(n) for n in new_pre[0:6]+[new_pre[8]]))+'\n')
+			new_pre=new_breakpoint_sorted[x]
+	if int(new_pre[6])>=3 and int(new_pre[7])>=3:
+		breakpoint_file.write(('\t'.join(str(n) for n in new_pre[0:6]+[new_pre[8]]))+'\n')
 	
 if __name__=='__main__':
 	breakpoint_sort(sys.argv[1])
